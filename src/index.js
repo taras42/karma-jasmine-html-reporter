7a07446a46ddd4a7478b0b67b0293975f1c27499
Yudha Setiawan <yudhasetiawan52@yahoo.com>
2016-07-19 19:46:22 +0700

    fix(config): File dependencies order to work with karma: ^1.0
    Fixes: #4

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
+var JASMINE_CORE_PATTERN = /(\/karma-jasmine\/)/i;
 var createPattern = function(path) {
   return {pattern: path, included: true, served: true, watched: false};
 };
 
 var initReporter = function(files,  baseReporterDecorator) {
+  var jasmineCoreIndex = 0;
 
   baseReporterDecorator(this);
 
-  files.unshift(createPattern(__dirname + '/lib/adapter.js'));
-  files.unshift(createPattern(__dirname + '/lib/html.jasmine.reporter.js'));
-  files.unshift(createPattern(__dirname + '/css/jasmine.css'));
+  files.forEach(function(file, index) {
+    if (JASMINE_CORE_PATTERN.test(file.pattern)) {
+      jasmineCoreIndex = index;
+    }
+  });
+
+  files.splice(++jasmineCoreIndex, 0, createPattern(__dirname + '/css/jasmine.css'));
+  files.splice(++jasmineCoreIndex, 0, createPattern(__dirname + '/lib/html.jasmine.reporter.js'));
+  files.splice(++jasmineCoreIndex, 0, createPattern(__dirname + '/lib/adapter.js'));
 };
 
 initReporter.$inject = ['config.files',  'baseReporterDecorator'];
